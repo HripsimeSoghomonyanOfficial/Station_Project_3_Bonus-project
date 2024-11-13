01cc56a9ac41c513f753f12e80c48ae046b08493
HripsimeSoghomon <hripsime.soghomonyan95@gmail.com>
2024-11-13 16:55:21 +0400

    Update Task.py

--- Task.py
+++ Task.py
@@ -9,9 +9,26 @@ def countdown_timer(hours, minutes, seconds):
         print(f"{hrs:02}:{mins:02}:{secs:02}", end='\r')
         time.sleep(1)
         total_seconds -= 1
-    print("Time's up!")
+    print("\nTime's up!")
+
+def get_time_input():
+    while True:
+        try:
+            time_input = input("Insert time to count down (h:m:s): ")
+            h, m, s = map(int, time_input.split(':'))
+            
+            if h < 0 or m < 0 or s < 0:
+                print("Error: Hours, minutes, and seconds must be non-negative integers.")
+                continue
+
+            if m >= 60 or s >= 60:
+                print("Error: Minutes and seconds must be within 0 and 59.")
+                continue
+
+            return h, m, s
+        except ValueError:
+            print("Invalid format. Please enter time in the format h:m:s, using integers.")
 
 if __name__ == "__main__":
-    time_input = input("Insert time to count down (h:m:s) ")
-    h, m, s = map(int, time_input.split(':'))
-    countdown_timer(h, m, s)
+    hours, minutes, seconds = get_time_input()
+    countdown_timer(hours, minutes, seconds)
